26cfd9a2789d9115e922c018b17e0119422c9417
Kevin Nowland <kevin.nowland@gmail.com>
2022-09-11 16:13:19 -0400

    feat: add Predictor class

--- make_prediction.py
+++ make_prediction.py
@@ -1,10 +1,12 @@
-import pickle
+from predictor import Predictor
 
-
-with open("clf.pickle", "rb") as f:
-    model = pickle.load(f)
 
 
 if __name__ == "__main__":
-    y = model.predict([[1, 1, 1, 1]])
-    print(y[0])
+
+    predictor = Predictor(".")
+    y = predictor.predict_one([1.2, 0.8, 2.4, 1.2])
+    print(y)
+
+    y_batch = predictor.predict_batch([[1.2, 0.8, 2.4, 1.2], [2.1, 1.4, 1.0, 3.2]])
+    print(y_batch)
