ef16b474289c6b28ef496b3c1191cb1ebd635365
Kevin Nowland <kevin.nowland@gmail.com>
2022-10-01 15:19:09 -0400

    tests: add e2e test for endpoint

---
+++ tests/e2e/test_endpoint.py
@@ -0,0 +1,19 @@
+import json
+
+import requests
+
+
+def test_ping():
+    r = requests.get("http://localhost:8080/ping")
+    assert r.status_code == 200
+    assert r.headers["Content-Type"] == "application/json"
+    assert r.json() == ""
+
+
+def test_predictions():
+    r = requests.post(
+        "http://localhost:8080/predictions", json=json.dumps({"x": 1, "y": 2})
+    )
+    assert r.status_code == 200
+    assert r.headers["Content-Type"] == "application/json"
+    assert "prediction" in r.json().keys()
