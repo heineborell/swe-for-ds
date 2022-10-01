ff32b5d665d901c58f37112accf450bbd2b4caad
Kevin Nowland <kevin.nowland@gmail.com>
2022-10-01 11:57:21 -0400

    feat: add data_processing module

---
+++ src/someproject/data_processing.py
@@ -0,0 +1,23 @@
+"""simulating some data processing"""
+import random
+import time
+
+
+class DataStore:
+    """a fake datastore connection"""
+
+    def __init__(self, database: str):
+        self.database = database
+
+    def get_data(self, data_id: int):
+        """pretend to get data
+
+        Simulates I/O bounded connection
+        """
+        time.sleep(0.1)
+        return random.randint(0, 10)
+
+
+def find_averages(data: list[int]) -> float:
+    """compute bounded computation"""
+    return sum(data) / len(data)
