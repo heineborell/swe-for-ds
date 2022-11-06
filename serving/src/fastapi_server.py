dc1d8495a3a1a7e3e7405c170cd29d8d8d39d8a0
kevinnowland <kevin.nowland@gmail.com>
2022-11-06 12:39:05 -0500

    Video15/frameworks (#6)
    * feat: add flask server with get route
    
    * feat: add post route to flask server
    
    * feat: add ping route
    
    * feat: add fastapi server
    
    * feat: move serving to separate folder
    
    * feat: move serving to separate folder
    
    * refactor: nest servers under src directory
    
    * docs: update readme
    
    * docs: update readme
    
    * docs: update readme
    
    * docs: update readme

---
+++ serving/src/fastapi_server.py
@@ -0,0 +1,32 @@
+import os
+
+from fastapi import FastAPI
+
+from someproject import IrisData, Prediction, Predictor
+
+app = FastAPI(title="iris_fastapi")
+predictor = Predictor(os.environ.get("IRIS_PICKLE_DIR", "./artifact"))
+
+
+@app.get("/ping")
+def ping():
+    return ""
+
+
+@app.get("/predictions", response_model=Prediction)
+def predictions_get(
+    sepal_length: float, sepal_width: float, petal_length: float, petal_width: float
+) -> Prediction:
+    iris_data = IrisData(
+        sepal_length=sepal_length,
+        sepal_width=sepal_width,
+        petal_length=petal_length,
+        petal_width=petal_width,
+    )
+
+    return predictor.predict_one(iris_data)
+
+
+@app.post("/predictions", response_model=Prediction)
+def predictions_post(iris_data: IrisData) -> Prediction:
+    return predictor.predict_one(iris_data)
---
+++ serving/src/flask_server.py
@@ -0,0 +1,92 @@
+import json
+import os
+
+from flask import Flask, Response, request
+from pydantic import ValidationError
+
+from someproject import IrisData, Predictor
+
+app = Flask("iris_flask")
+predictor = Predictor(os.environ.get("IRIS_PICKLE_DIR", "./artifact"))
+
+
+def get_validation_error_response(e: ValidationError) -> Response:
+    """compares required and received, returning missing args"""
+    error_data = {
+        "event": "ValidationError",
+        "error": str(e),
+    }
+
+    return Response(
+        response=json.dumps(error_data),
+        status=422,
+        mimetype="application/json",
+    )
+
+
+def get_model_error_response(e: Exception) -> Response:
+    """return 500 for other error"""
+    error_data = {
+        "event": "ModelError",
+        "error": str(e),
+    }
+
+    return Response(
+        response=json.dumps(error_data),
+        status=500,
+        mimetype="application/json",
+    )
+
+
+@app.get("/ping")
+def ping():
+    return Response(
+        response="",
+        status=200,
+        mimetype="text/plain",
+    )
+
+
+@app.get("/predictions")
+def predictions_get():
+
+    try:
+        iris_data = IrisData(
+            sepal_length=request.args.get("sepal_length"),
+            sepal_width=request.args.get("sepal_width"),
+            petal_length=request.args.get("petal_length"),
+            petal_width=request.args.get("petal_width"),
+        )
+    except ValidationError as e:
+        return get_validation_error_response(e)
+
+    try:
+        prediction = predictor.predict_one(iris_data)
+    except Exception as e:
+        return get_model_error_response(e)
+
+    return Response(
+        response=prediction.json(),
+        status=200,
+        mimetype="application/json",
+    )
+
+
+@app.post("/predictions")
+def predictions_post():
+
+    try:
+        iris_data = IrisData(**request.json)
+    except ValidationError as e:
+        return get_validation_error_response(e)
+
+    try:
+        prediction = predictor.predict_one(iris_data)
+    except Exception as e:
+        return get_model_error_response(e)
+
+    return Response(
+        response=prediction.json(),
+        status=200,
+        mimetype="application/json",
+    )
